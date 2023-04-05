fe4b42984c36abe4e7782d5d50a513dc5b26ddeb
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-04-05 23:50:00 +0200

    WORKING AND FIX SOME BUGS!

--- client/index.js
+++ client/index.js
@@ -1,35 +1,51 @@
+#!/usr/bin/env node
 const socketIoClient=require("socket.io-client");
+const fs=require("fs");
 
 const port=process.env.socketTCP_port||3245;
-const getFiles={};
-const files={};
+const files=new Map();
 
 const socket=socketIoClient(`http://localhost:${port}`);
 
 socket.on("connect",()=>{
-	console.log("connected as"+socket.id);
+	console.log("connected as "+socket.id);
 	if(process.argv[2]){
 		const path=process.argv[2];
+		const output=process.argv[3]||"file.bin";
 		console.log("get file "+path);
-		socket.emit("get-file",path);
+		socket.emit("get-file",path,data=>{
+			files.set(data.id,{
+				buffer: new Uint8Array(data.size),
+				file: data.file,
+				finished: false,
+				id: data.id,
+				output,
+				path,
+				size: data.size,
+			});
+		});
 	}
 });
 socket.on("disconnect",()=>console.log("disconnect"));
-socket.on("get-file",(path,chunk,cb)=>{
-	if(typeof(chunk)=="boolean"&&chunk){
-		const file=getFiles[path];
-		files[path]=file;
-		console.log(path+" length "+file.length);
-		delete getFiles[path];
+socket.on("get-file",(id,startIndex,chunk,cb)=>{
+	const entry=files.get(id);
+	if(!entry||entry.finished){
+		console.log(!entry?`file with id "${id}" not found`:`file ${entry.path} wurde schon abgeschlossen`);
 		return;
 	}
-	if(!getFiles[path]){
-		getFiles[path]=[];
+	if(typeof(startIndex)=="boolean"&&startIndex){
+		console.log(`\nDatei empfangen! wird nun unter "${entry.output}" gespeichert Größe beträgt ${entry.size} Bytes`);
+		entry.finished=true;
+		files.set(id,entry);
+		fs.writeFileSync(entry.output,Buffer.from(entry.buffer));
+		return;
 	}
-	getFiles[path]=[
-		...getFiles[path],
-		...chunk,
-	];
-	console.log("add chunk for "+path+" with size "+chunk.length);
+
+	for(let index=0; index<chunk.length+1; index++){
+		entry.buffer[startIndex+index]=chunk[index];
+	}
+
+	files.set(id,entry);
+	process.stdout.write(`${entry.file} Add Chunk: ${String(startIndex).padStart(String(entry.size).length,"0")}-${String(startIndex+chunk.length).padStart(String(entry.size).length,"0")} Chunk Address, ${String(startIndex).padStart(String(entry.size).length,"0")}/${entry.size} Bytes, ${chunk.length} Chunk Größe Bytes\r`);
 	cb(true);
 });
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+#!/usr/bin/env node
 const socketIo=require("socket.io");
 const fs=require("fs");
 
 const port=process.env.socketTCP_port||3245;
-const sendBytesPerChunk=0xff;
+const sendBytesPerChunk=Number(process.argv[2]||1024);
 
 function readFile(path){return new Promise((async (resolve,reject)=>{
 	fs.readFile(path,(error,buffer)=>{
@@ -18,30 +19,33 @@ const io=socketIo(port,{
 });
 
 io.on("connection",socket=>{
-	socket.on("get-file",(async path=>{
+	socket.on("get-file",(async (path,callback)=>{
 		let buffer;
 		try{
 			buffer=await readFile(path);
 		}
-		catch(e){
-			/*cb({
-				type: "error",
-				code: e,
-				consoleError: `file '${path}' can't read`,
-			});*/
-		};
-		/*cb({
-			type: "ok",
-			consoleLog: `file '${path}' was read length is ${buffer.length} Bytes`,
-		});*/
+		catch(e){return;}
 
-		const realBuffer=buffer;
-		buffer=JSON.parse(JSON.stringify(buffer)).data;	// make it to array buffer
 		// array buffer: [99, 111, 110, 115, 111, 108, ...]
 		// buffer: <Buffer 63 6f 6e 73 6f ...>
 		// array buffer supports .map and other functions
 
-		const fileSize=buffer.length-1;
+		const file=path.split("/").pop();
+		const fileSize=buffer.length;
+		let sendId;
+		{
+			const pathHex=Buffer.from(path,"utf-8").toString("hex");
+			const time=Date.now();
+			sendId=`${pathHex}_${fileSize}_${time}`;
+		}
+		callback({
+			file,
+			id: sendId,
+			path,
+			size: fileSize,
+		});
+		const startTime=Date.now();
+		console.log(`sending file "${path}" size ${fileSize} bytes to client "${socket.id}"...`);
 		for(let index=0; index<fileSize+sendBytesPerChunk; index+=sendBytesPerChunk){
 			if(index>fileSize){
 				break;
@@ -52,14 +56,17 @@ io.on("connection",socket=>{
 				end=fileSize;
 			}
 
-			const chunk=buffer.filter((byte,i)=>i>=index&&i<=end)
-			
+			const chunk=buffer.slice(index,end);
 			await new Promise(resolve=>{ // send chunk to client
-				socket.emit("get-file",path,chunk,resolve);
+				socket.emit("get-file",sendId,index,chunk,resolve);
 			});
-			//socket.emit("get-file",path,chunk,()=>console.log("chunk sended"));
-			//console.log(chunk);
+			process.stdout.write(`Datei "${file}": ${String(index).padStart(String(fileSize).length,"0")}/${fileSize} wurden gesendet ...\r`);
 		}
-		socket.emit("get-file",path,true);
+		socket.emit("get-file",sendId,true,null);
+		const endTime=Date.now();
+		const timeSec=Math.round(((endTime-startTime)/1000)*200)/200;
+		console.log(`Datei: "${path}" with size ${fileSize} Bytes. Wurde übertragen in ${timeSec} Sekunden, ${Math.round(timeSec/60*100)/100} Minuten`);
 	}));
 });
+
+console.log(`Socket Server ist Online auf Port ${port}!\nChunk Größe beträgt ${sendBytesPerChunk} Bytes!\n`);
